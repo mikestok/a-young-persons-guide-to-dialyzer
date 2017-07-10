0eb0dd5bcd4d9729d1dfb9e94f5d9808d57dc927
Mike Stok <mike@stok.ca>
2017-07-09 20:02:37 -0400

    Make changes to satisfy dialyzer and add some tests
    This commit eliminates the problems detected by `dialyzer`.
    
    I added a doctest to illustrate the "happy path," and added a @spec
    annotation so that the input and output types appear in documentation.
    
    The test suite was updated with a couple of "useful" tests.
    
    One thing I noticed was that Timex.parse!/1 happily parsed "2015-02-29",
    but Erlang's :calendar.valid_date({2015, 2, 29}) returns false. That's
    not in the scope of this exercise to fix!

--- straightforward/lib/straightforward.ex
+++ straightforward/lib/straightforward.ex
@@ -1,7 +1,15 @@
 defmodule Straightforward do
-  def string_to_date(date_string) do
+  @doc """
+  Parse a string containing a date (yyyy-mm-dd) and return a Date if it is
+  valid. Raise an error if it is not valid.
+
+      iex> Straightforward.string_to_date("2017-07-09")
+      ~D[2017-07-09]
+  """
+  @spec string_to_date(String.t) :: Date.t | no_return
+  def string_to_date(date_string) when is_binary(date_string) do
     date_string
-    |> Timex.parse("{YYYY}-{0M}-{D}")
-    |> DateTime.to_date()
+    |> Timex.parse!("{YYYY}-{0M}-{D}")
+    |> Timex.to_date()
   end
 end
--- straightforward/test/straightforward_test.exs
+++ straightforward/test/straightforward_test.exs
@@ -2,7 +2,17 @@ defmodule StraightforwardTest do
   use ExUnit.Case
   doctest Straightforward
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "string_to_date" do
+    test "raises a parse error for an illegal but 'valid looking' date" do
+      assert_raise Timex.Parse.ParseError, fn ->
+        Straightforward.string_to_date("2017-07-32")
+      end
+    end
+
+    test "raises a parse error for an illegal date" do
+      assert_raise Timex.Parse.ParseError, fn ->
+        Straightforward.string_to_date("banana")
+      end
+    end
   end
 end
